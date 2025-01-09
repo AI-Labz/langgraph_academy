e14c5def0a91a87762ba67a84473032a4debc6f7
Guilherme M. Toso <gui.m.toso@gmail.com>
2025-01-09 13:40:23 -0300

    feat: add module 3

---
+++ langgraph_academy/module_3/l3_edit_state/edit_state.py
@@ -0,0 +1,66 @@
+﻿from langchain_ollama import ChatOllama
+from langgraph.graph import StateGraph, START, END, MessagesState
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langgraph.prebuilt import tools_condition, ToolNode
+
+llm = ChatOllama(
+    model="llama3.1-tool",
+    base_url="http://localhost:11434",
+    temperature=0.8,
+    max_tokens=1000
+)
+
+def multiply(a:int, b:int)-> int:
+    """Multiply a and b.
+    
+    Args:
+        a: first int
+        b: second int
+    """
+    return a*b
+
+def add(a:int, b:int) -> int:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a+b
+
+def divide(a:int, b:int) -> float:
+    """Divide a and b.
+    
+    Args:
+        a: first int
+        b: second: int
+    """
+    return a/b
+
+tools = [multiply, add, divide]
+llm_with_tools = llm.bind_tools(tools)
+
+# System Message
+system_message = SystemMessage(content="You are a helpful AI assistant tasked with performing arithmatic on a set of inputs.")
+
+# Assistant
+def assistant(state: MessagesState):
+    return {"messages": llm_with_tools.invoke([system_message] + state["messages"])}
+
+# Graph
+builder = StateGraph(MessagesState)
+
+# Add the nodes
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+
+# Add the edges
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges("assistant", tools_condition)
+builder.add_edge("tools", "assistant")
+
+
+# Compile
+memory = MemorySaver()
+graph = builder.compile(interrupt_before=["assistant"], checkpointer=memory)
---
+++ langgraph_academy/module_3/l4_dynamic_breakpoints/dynamic_breakpoints.py
@@ -0,0 +1,38 @@
+﻿from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.errors import NodeInterrupt
+from langgraph.checkpoint.memory import MemorySaver
+
+class State(TypedDict):
+    input: str
+
+
+def step_1(state: State) -> State:
+    print("---- Step 1 ----")
+    return state
+
+def step_2(state: State) -> State:
+    print("---- Step 2 ----")
+    if len(state['input']) > 5:
+        raise NodeInterrupt(f"Input is longer than 5 characters: {state['input']}")
+    return state
+
+def step_3(state: State) -> State:
+    print("---- Step 3 ----")
+    return state
+
+
+builder = StateGraph(State)
+
+builder.add_node("step_1", step_1)
+builder.add_node("step_2", step_2)
+builder.add_node("step_3", step_3)
+
+builder.add_edge(START, 'step_1')
+builder.add_edge("step_1", "step_2")
+builder.add_edge("step_2","step_3")
+builder.add_edge("step_3", END)
+
+memory = MemorySaver()
+
+graph = builder.compile(checkpointer=memory)
